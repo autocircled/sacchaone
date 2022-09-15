abb8c2773e3ff748ee605e8ae6cb57604763f906
Moktadir Rahman <autocircled@gmail.com>
2022-09-15 18:30:55 +0600

    color element expand-closing issue solved

--- assets/js/customizer/custom-separator.js
+++ assets/js/customizer/custom-separator.js
@@ -1,19 +1,22 @@
 (function ($) {
-
-    $( 'body' ).on( 'click', '.sacchaone-toggle-control', function(){
-        // console.log($(this).attr('target_ids'));
-        let $this = $(this);
-        var raw_ids = $(this).attr('target_ids').split(" ");
-        if( $($this).hasClass('showing') ){
-            $($this).removeClass('showing');
-            $(raw_ids).each(function(i,v){
-                $( "#customize-control-" + v ).hide();
-            });
-        }else{
-            $($this).addClass('showing');
-            $(raw_ids).each(function(i,v){
-                $( "#customize-control-" + v ).show();
-            });
-        }
-    });
+	$("body").on(
+		"click",
+		".sacchaone-toggle-control, .toggle-text",
+		function () {
+			let parentEl = $(this).parent();
+			let $this = $(parentEl).find(".sacchaone-toggle-control");
+			var raw_ids = $($this).attr("target_ids").split(" ");
+			if ($($this).hasClass("showing")) {
+				$($this).removeClass("showing");
+				$(raw_ids).each(function (i, v) {
+					$("#customize-control-" + v).hide();
+				});
+			} else {
+				$($this).addClass("showing");
+				$(raw_ids).each(function (i, v) {
+					$("#customize-control-" + v).show();
+				});
+			}
+		}
+	);
 })(jQuery);
