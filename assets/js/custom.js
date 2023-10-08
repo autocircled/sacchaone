8cea4800e43adc003d3a7b1ef3de879281f80eaf
Moktadir Rahman <autocircled@gmail.com>
2023-10-08 14:26:40 +0600

    jquery dependency updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,65 +1,153 @@
-/**************/
-// sacchaone custom.js
-/**************/
-(function ($) {
- "use strict";
-  //nav search
-  $('body').on('click', '.nav-bar-items .search-item', function(){
-    if( $('.nav-search').hasClass('search-active') ){
-      $('.nav-search').removeClass('search-active');
-    }else{
-      $('.nav-search').addClass('search-active');
-    }
-
-    if( $('.nav-bar-items .search-item').hasClass('search-btn-active') ){
-      $('.nav-bar-items .search-item').removeClass('search-btn-active');
-    }else{
-      $('.nav-bar-items .search-item').addClass('search-btn-active');
-    }
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  "use strict";
 
   //scroll to top
-    $("#scroll_to_top").hide();
-    $("#scroll_to_top").on("click",function(e) {
+  var scrollToTopBtn = document.getElementById('scroll_to_top');
+  if (scrollToTopBtn) {
+    scrollToTopBtn.style.display = 'none';
+
+    scrollToTopBtn.addEventListener('click', function(e) {
       e.preventDefault();
-      $("html, body").animate({ scrollTop: 0 }, "slow");
+      window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 
-    $(window).on( 'scroll', function(){
-      var scrollheight =400;
-      if( $(window).scrollTop() > scrollheight ) {
-        $("#scroll_to_top").fadeIn();
-        $("#scroll-to-top").addClass("scroll-visible");
-      }
-      else {
-        $("#scroll_to_top").fadeOut();
-        $("#scroll_to_top").removeClass("scroll-visible");
+    window.addEventListener('scroll', function() {
+      var scrollheight = 400;
+      if (window.scrollY > scrollheight) {
+        scrollToTopBtn.style.display = 'block';
+        scrollToTopBtn.classList.add('scroll-visible');
+      } else {
+        scrollToTopBtn.style.display = 'none';
+        scrollToTopBtn.classList.remove('scroll-visible');
       }
     });
-	
-    $("a[href^=\\#]").on( 'click', function(event){
-        event.preventDefault();
-        if( ! $(this).hasClass( SACCHA_DATA.scroll_spy_selector ) ){
-          $('html,body').animate({scrollTop:($(this.hash).offset().top - 150)}, 500);
-        }
-    });
-	
-	$(window).on( 'scroll', function (event) {
-		stickyNav();
-	});
-	$(window).on( 'load', function (event) {
-		stickyNav();
-	});
-
-  function stickyNav(){
-    var scrollValue = $(window).scrollTop();
-		if (scrollValue > 120 && $('body').hasClass('sticky-nav-enabled')) {
-			$('body').addClass('sticky-nav');
-			$('.nav-header').addClass('affix');
-		} else{
-      $('body').removeClass('sticky-nav');
-			$('.nav-header').removeClass('affix');
-		}
   }
-	
-})(jQuery);
+
+
+  // plugin hack for [accondion faq builder]
+  document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+    anchor.addEventListener('click', function(event) {
+      event.preventDefault();
+      if (!this.classList.contains('scroll_spy_selector')) {
+        var targetElement = document.querySelector(this.hash);
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop - 150,
+            behavior: 'smooth'
+          });
+        }
+      }
+    });
+  });
+
+  window.addEventListener('scroll', stickyNav);
+  window.addEventListener('load', stickyNav);
+
+  function stickyNav() {
+    var scrollValue = window.scrollY;
+    var body = document.body;
+
+    if (scrollValue > 120 && body.classList.contains('sticky-nav-enabled')) {
+      body.classList.add('sticky-nav');
+      document.querySelector('.nav-header').classList.add('affix');
+    } else {
+      body.classList.remove('sticky-nav');
+      document.querySelector('.nav-header').classList.remove('affix');
+    }
+  }
+
+  var sacchaModal = sacchaModal || {};
+
+  const body = document.querySelector('body');
+  const menu = document.getElementById('site-navigation-mobile');
+  const search = document.getElementById('search-modal');
+  const openMenuModal = document.querySelector('.navbar-toggler-open');
+  const openSearchModal = document.querySelector('.search-toggler-open');
+  const backdrop = document.createElement('div');
+  let closeBtnType = false;
+  sacchaModal.popup = {
+      init: function() {
+          openMenuModal.addEventListener('click', this.open,);
+          openSearchModal.addEventListener('click', this.open);
+
+          const closeModalBtn = document.querySelectorAll('.saccha-btn-close');
+          closeModalBtn.forEach(closeBtn => closeBtn.addEventListener('click', this.close));
+
+          // close modal on click backdrop
+          backdrop.addEventListener('click', this.close);
+
+          menu.addEventListener('keydown', this.trapFocus);
+          search.addEventListener('keydown', this.trapFocus);
+
+          // Optional: Close modal on Escape key press
+          document.addEventListener('keydown', function (event) {
+              if (event.key === 'Escape') {
+                  sacchaModal.popup.close();
+              }
+          });
+
+      },
+      open: function() {
+          closeBtnType = this.getAttribute('data-target');
+          backdrop.className = "modal-backdrop fade show";
+          body.classList.add( 'modal-open' );
+          body.style.paddingRight = "17px";
+          if ( closeBtnType === '#site-navigation-mobile' ) {
+              menu.classList.add( 'show' );
+              menu.style.display = 'block';
+              menu.focus();
+          } else if ( closeBtnType === '#search-modal' ) {
+              search.classList.add( 'show' );
+              search.style.display = 'block';
+              search.focus();
+          }
+          body.appendChild(backdrop);
+      },
+
+      close: function() {
+          body.classList.remove('modal-open');
+          menu.classList.remove('show');
+          search.classList.remove('show');
+          menu.style.display = 'none';
+          search.style.display = 'none';
+          body.style.paddingRight = "";
+          closeBtnType === '#site-navigation-mobile' ? openMenuModal.focus() : closeBtnType === '#search-modal' ? openSearchModal.focus() : false;
+
+          body.removeChild(backdrop);
+      },
+
+      /**
+       * Focused on menu modal
+       * @param {tab} event 
+       */
+      trapFocus: function(event) {
+          const targetEl = closeBtnType === '#site-navigation-mobile' ? menu : search;
+          const menuLinks = Array.from( targetEl.querySelectorAll( 'a, button, input' ) );
+          const focusableElements = menuLinks.filter(link => !link.hasAttribute('disabled'));
+
+          // Check if Tab key is pressed
+          if (event.key === 'Tab') {
+              // Prevent the default tab behavior
+              event.preventDefault();
+
+              // Find the index of the currently focused element
+              const focusedIndex = focusableElements.indexOf(document.activeElement);
+
+              // Determine the next index for focus
+              const nextIndex = (focusedIndex + 1) % focusableElements.length;
+
+              // Set focus on the next element in the array
+              focusableElements[nextIndex].focus();
+          }
+      }
+  }
+
+  // If modal not exists we won't go funther.
+  if ( ! menu || ! search ) {
+      return false;
+  }
+
+  // // Now we are safe. Call our awesome modal.
+  sacchaModal.popup.init();
+
+});
